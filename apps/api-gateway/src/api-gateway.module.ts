36f364273826f27c6e58e8e7ff2e8d8b0480cd45
PhungMHieu <kiritosad41@gmail.com>
2025-11-01 14:24:32 +0700

    refactor: remove transaction aggregator module and its related files

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
-import { TransactionAggregatorModule } from './transaction-aggregator/transaction-aggregator.module';
 import { AuthModule } from '@app/shared/auth';
 
 @Module({
@@ -12,7 +11,6 @@ import { AuthModule } from '@app/shared/auth';
       envFilePath: '.env',
     }),
     AuthModule,
-    TransactionAggregatorModule,
   ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
